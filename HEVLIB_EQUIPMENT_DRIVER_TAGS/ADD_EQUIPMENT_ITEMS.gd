7e323d6c69824dfd35223b7e3fe3093889f043a3
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-08-23 19:37:52 -0700

    updated to 1.1.1 to add support for HL 1.6.8

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/ADD_EQUIPMENT_ITEMS.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+const cradle_left = {
+	"system":"SYSTEM_CRADLE-L",
+	"name_override":"SYSTEM_CRADLE",
+	"manual":"SYSTEM_CRADLE_MANUAL",
+	"specs":"SYSTEM_CRADLE_SPECS",
+	"price":2500,
+	"alignment":"ALIGNMENT_LEFT",
+	"test_protocol":"detach",
+	"equipment_type":"EQUIPMENT_CARGO_CONTAINER",
+	"slot_type":"HARDPOINT"
+}
+const cradle_right = {
+	"system":"SYSTEM_CRADLE-R",
+	"name_override":"SYSTEM_CRADLE",
+	"manual":"SYSTEM_CRADLE_MANUAL",
+	"specs":"SYSTEM_CRADLE_SPECS",
+	"price":2500,
+	"alignment":"ALIGNMENT_RIGHT",
+	"test_protocol":"detach",
+	"equipment_type":"EQUIPMENT_CARGO_CONTAINER", 
+	"slot_type":"HARDPOINT"
+}
