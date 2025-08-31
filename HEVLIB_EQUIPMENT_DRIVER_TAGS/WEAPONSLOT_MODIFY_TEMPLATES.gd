d9cd1c05bb3b1fd8594abb87171f6b10e65d0aea
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-08-31 00:15:38 -0700

    added cradle positioning templates

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/WEAPONSLOT_MODIFY_TEMPLATES.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const WEAPONSLOT_MODIFY_TEMPLATES = {
+	"CRADLES":{
+		"equipment":[
+			"SYSTEM_CRADLE-L",
+			"SYSTEM_CRADLE-R"
+		]
+	},
+	"CARGO_CONTAINERS":{
+		"equipment":[
+			"SYSTEM_CRADLE-L",
+			"SYSTEM_CRADLE-R"
+		]
+	},
+}
