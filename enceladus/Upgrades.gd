3d48d1fdf62e7f5a02d9649b0a2d9e4f176b71ce
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-09-01 01:26:44 -0700

    updated showReliability with proper values, and added support for gimballed/turreted equipment and cradles

--- enceladus/Upgrades.gd
+++ enceladus/Upgrades.gd
@@ -2,6 +2,9 @@ extends "res://enceladus/Upgrades.gd"
 
 var _showReliability_system = null
 
+var disable_when_false = PoolStringArray(["damageModel"])
+var disable_when_true = PoolStringArray([])
+
 func previewShipSystem(slot,system,control=""):
 	.previewShipSystem(slot,system,control)
 	if slot == null or system is int or system is float:
@@ -22,8 +25,25 @@ func _process(delta):
 	if n == null or n is InstancePlaceholder:
 		return
 	_showReliability_system = null  # OK, it's instantiated.
-
+	
+	var enabled = true
+	var txt = ""
+	
 	for p in n.get_property_list():
+		if p.name == "weaponPath":
+			var path = str(n.get(p.name)).split(n.systemName)[1].lstrip("/")
+			n = n.get_node(path)
+	
+	for p in n.get_property_list():
+		if p.name in disable_when_false:
+			var check = n.get(p.name)
+			if check == false:
+				enabled = false
+		if p.name in disable_when_true:
+			var check = n.get(p.name)
+			if check == true:
+				enabled = false
+		
 		if p.name.begins_with("damage") and p.name.ends_with("Capacity"):
 			var name = p.name.substr(6, p.name.length() - 8)
 			var units = ""
@@ -42,6 +62,20 @@ func _process(delta):
 					name = "Choke resilience"
 					units = "kN"
 					factor = 1 / 1000.0
+				
+				# Mass driver and Mike specifics
+				"damageFocusCapacity":
+					name = "Thermal resilience"
+					units = "MJ"
+					factor = 1 / 1000.0
+				
+				# Laser specifics
+				"damagePumpDamageCapacity":
+					name = "Short-circuit resilience"
+					units = "MJ"
+					factor = 1 / 1000000.0
+				
+				
 
 				# These aren't visible in the game right now, because
 				# you can't buy / customize computers.
@@ -71,9 +105,22 @@ func _process(delta):
 					units = "MJ"
 					factor = 1 / 1000000.0
 				
+				# Also fusion reactor damage
+				"damageCoilsCapacity":
+					name = "Acceleration dampening"
+					units = "m/s"
+				"damageCoolantCapacity":
+					name = "Impact resilience"
+					units = "kN"
+					factor = 1 / 1000.0
+				"damageLaserCapacity":
+					name = "Thermal resilience"
+					units = "MJ"
+					factor = 1 / 1000000.0
 				
-			specsLabel.text = tr(specsLabel.text) + "\n%s: %s %s" % [name, _showReliability_formatFloat(n.get(p.name) * factor), units]
-
+			txt = txt + "\n%s: %s %s" % [name, _showReliability_formatFloat(n.get(p.name) * factor), units]
+	if enabled:
+		specsLabel.text = TranslationServer.translate(specsLabel.text) + txt
 func _showReliability_formatFloat(f):
 	# Format without unneeded decimals
 	var s = "%f" % f
