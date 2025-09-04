7548aa100707519b2eeaa4fb4555a3b0133534ed
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-09-04 16:45:56 -0700

    created a new potential solution to the HUD keybind issue, and swapped process to physics process to make more efficient

--- hud/OMS.gd
+++ hud/OMS.gd
@@ -3,9 +3,11 @@ extends "res://hud/OMS.gd"
 var _diveClock: Label
 var _diveClockGame
 
+var ship
+
 func _ready():
 	Debug.l("DIVE-CLOCK: Readying hud/OMS")
-
+	ship = get_parent().get_parent()
 	var container = find_node("CREW_OCCUPATION_MECHANIC", true)
 	_diveClock = Label.new()
 	_diveClock.modulate = Color("#00ff00")
@@ -14,7 +16,13 @@ func _ready():
 	_diveClockGame = get_tree().root.get_node("Game")
 
 
-func _process(delta):
+func _input(event):
+	if !ship.cutscene and ship.isPlayerControlled():
+		if Input.is_action("velocityplus_toggle_hud"):
+			get_parent().visible = !get_parent().visible
+
+
+func _physics_process(delta):
 	var now = CurrentGame.getInGameTimestamp()
 	var text = CurrentGame.timeToString(now)
 	if _diveClockGame != null:
