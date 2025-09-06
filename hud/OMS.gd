a36f9a50a1d9d6fba3f2e062cb8eeae22e711ebe
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-09-06 09:36:27 -0700

    fixed mpu toggle for modded mpu & made it more efficient

--- hud/OMS.gd
+++ hud/OMS.gd
@@ -36,21 +36,12 @@ func _input(event):
 		if event.is_action_pressed("velocityplus_toggle_hud"):
 			get_parent().visible = !get_parent().visible
 		if event.is_action_pressed("velocityplus_toggle_mpu"):
-			var has_processor = false
-			for processor in MPUs:
-				if is_instance_valid(processor):
-					processor.enabled = !processor.enabled
-					has_processor = true
-				else:
-					print("invalid instance")
-			if not has_processor:
-				MPUs = []
-				for child in ship.get_children():
-					var scriptobj = child.get_script()
-					if scriptobj != null:
-						var script = scriptobj.get_path()
-						if script == "res://ships/modules/MineralProcessingUnit.gd":
-							MPUs.append(child)
+			var current_mpu = ship.getConfig("cargo.equipment")
+			for node in ship.get_children():
+				if "systemName" in node:
+					var nname = node.name
+					if node.systemName == current_mpu:
+						node.enabled = !node.enabled
 
 func _physics_process(delta):
 	if add_dive_time:
