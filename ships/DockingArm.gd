58e12fc01c1e3239627af0bdaf21eaa744af3844
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-11-28 18:53:55 -0800

    added extra check to armfocus to ensure that hardLock is not set

--- ships/DockingArm.gd
+++ ships/DockingArm.gd
@@ -5,10 +5,12 @@ const ConfigDriver = preload("res://HevLib/pointers/ConfigDriver.gd")
 func isValidTarget(body: RigidBody2D) -> bool:
 	var result = .isValidTarget(body)
 	var allow = ConfigDriver.__get_value("VelocityPlus","VP_SHIPS","arm_focuses_to_targeted_object")
-	if result and allow:
-		if Tool.claim(ship):
-			if ship.autopilotVelocityOffsetTarget and body != ship.autopilotVelocityOffsetTarget:
-				# Debug.l("ARMFocus: Ignoring %s, want %s instead" % [body, ship.autopilotVelocityOffsetTarget])
-				result = false
-			Tool.release(ship)
+	if not hardLock:
+		if result:
+			if allow:
+				if Tool.claim(ship):
+					if ship.autopilotVelocityOffsetTarget and body != ship.autopilotVelocityOffsetTarget:
+						# Debug.l("ARMFocus: Ignoring %s, want %s instead" % [body, ship.autopilotVelocityOffsetTarget])
+						result = false
+					Tool.release(ship)
 	return result
