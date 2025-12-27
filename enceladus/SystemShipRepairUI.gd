ee6e0cd3ea25238a0be0048a1597071e4d799bb8
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-12-26 22:07:53 -0800

    Update SystemShipRepairUI.gd

--- enceladus/SystemShipRepairUI.gd
+++ enceladus/SystemShipRepairUI.gd
@@ -2,10 +2,13 @@ extends "res://enceladus/SystemShipRepairUI.gd"
 
 const ConfigDriver = preload("res://HevLib/pointers/ConfigDriver.gd")
 
+var allow_hide = true
+
 func updateStatuses():
 	.updateStatuses()
-	if system.status >= 99.5 and ConfigDriver.__get_value("VelocityPlus","VP_ENCELADUS","hide_unrepairable_equipment"):
-		visible = false
-	else:
-		visible = true
+	if ConfigDriver.__get_value("VelocityPlus","VP_ENCELADUS","hide_unrepairable_equipment") and allow_hide:
+		if system.status >= 99.5:
+			visible = false
+		else:
+			visible = true
 	
