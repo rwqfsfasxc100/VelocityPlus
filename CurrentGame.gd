e6e89c4e2452a4a23bdd86d99099c479faebec00
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-02-12 23:14:51 -0800

    Update CurrentGame.gd

--- CurrentGame.gd
+++ CurrentGame.gd
@@ -1,13 +1,26 @@
 extends "res://CurrentGame.gd"
 
-const VPCFG = preload("res://HevLib/pointers/ConfigDriver.gd")
+var pointersVPEMP
+
+func _ready():
+	yield(get_tree(),"idle_frame")
+	pointersVPEMP = get_tree().get_root().get_node_or_null("HevLib~Pointers")
+	pointersVPEMP.ConfigDriver.__establish_connection("updateValues",self)
+	updateValues()
+
+func updateValues():
+	if pointersVPEMP:
+		show_shipped_cargo_value = pointersVPEMP.ConfigDriver.__get_value("VelocityPlus","VP_RING","show_shipped_cargo_value")
+		astrogation_tracking_time_modifier = pointersVPEMP.ConfigDriver.__get_value("VelocityPlus","VP_CREW","astrogation_tracking_time_modifier")
 
 var this_dive_transactions_gain = 0
 var this_dive_transactions_spent = 0
 
+var show_shipped_cargo_value = true
+var astrogation_tracking_time_modifier = true
 func logEvent(type: String, details = {}):
 	.logEvent(type,details)
-	if VPCFG.__get_value("VelocityPlus","VP_RING","show_shipped_cargo_value"):
+	if show_shipped_cargo_value:
 		if type == "LOG_EVENT_DIVE":
 			if "LOG_EVENT_DETAILS_TRADE_SELL" in details:
 				var data = details["LOG_EVENT_DETAILS_TRADE_SELL"]
@@ -28,6 +41,6 @@ func logEvent(type: String, details = {}):
 
 func deriveStats(stats):
 	var s = .deriveStats(stats)
-	var multi = VPCFG.__get_value("VelocityPlus","VP_CREW","astrogation_tracking_time_modifier")
+	var multi = astrogation_tracking_time_modifier
 	stats.CREW_STATS_ASTROGATOR_TIME = max(12 * 3600, 24 * 3600 * (stats.CREW_OCCUPATION_ASTROGATOR.experience * 30 * 3 * (multi / 90)))
 	return s
