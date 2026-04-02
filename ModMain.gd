6f67a6a92a58f88010e212be0efd3aefe2042b21
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-02 11:55:06 -0700

    updated updateTL

--- ModMain.gd
+++ ModMain.gd
@@ -168,32 +168,47 @@ func updateTL(path:String, delim:String = ",", useRelativePath:bool = true, full
 		path = str(modPath + path)
 	l("Adding translations from: %s" % path)
 	var tlFile:File = File.new()
-	tlFile.open(path, File.READ)
-
+	var err = tlFile.open(path, File.READ)
+	
+	if err != OK:
+		return
+	
 	var translations := []
 	
 	var translationCount = 0
 	var csvLine := tlFile.get_line().split(delim)
+	
 	if fullLogging:
 		l("Adding translations as: %s" % csvLine)
 	for i in range(1, csvLine.size()):
 		var translationObject := Translation.new()
 		translationObject.locale = csvLine[i]
 		translations.append(translationObject)
-
+	
 	while not tlFile.eof_reached():
-		csvLine = tlFile.get_csv_line(delim)
-
-		if csvLine.size() > 1:
+		var line = tlFile.get_line()
+		if line.begins_with("#"):
+			continue
+		csvLine = line.split(delim)
+		var size = csvLine.size()
+		if size > 1:
+			if size > 2:
+				var i = 0
+				while i < size:
+					if csvLine[i].ends_with("\\") and i < size:
+						csvLine[i] = csvLine[i].rstrip("\\") + delim + csvLine[i + 1]
+						csvLine.remove(i + 1)
+						size -= 1
+					i += 1
 			var translationID := csvLine[0]
-			for i in range(1, csvLine.size()):
+			for i in range(1, size):
 				translations[i - 1].add_message(translationID, csvLine[i].c_unescape())
 			if fullLogging:
 				l("Added translation: %s" % csvLine)
 			translationCount += 1
-
+	
 	tlFile.close()
-
+	
 	for translationObject in translations:
 		TranslationServer.add_translation(translationObject)
 	l("%s Translations Updated" % translationCount)
