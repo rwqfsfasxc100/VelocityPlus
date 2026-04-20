65ef1e4606b121791d50f2290c70193188aac531
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-04-19 21:59:44 -0700

    added natural time skip

--- CurrentGame.gd
+++ CurrentGame.gd
@@ -2,6 +2,7 @@ extends "res://CurrentGame.gd"
 
 var pointersVPEMP
 
+var file = File.new()
 func _ready():
 	yield(get_tree(),"idle_frame")
 	pointersVPEMP = get_tree().get_root().get_node_or_null("HevLib~Pointers")
@@ -12,12 +13,14 @@ func vp_shippedgoods_UV():
 	if pointersVPEMP:
 		show_shipped_cargo_value = pointersVPEMP.ConfigDriver.__get_value("VelocityPlus","VP_RING","show_shipped_cargo_value")
 		astrogation_tracking_time_modifier = pointersVPEMP.ConfigDriver.__get_value("VelocityPlus","VP_CREW","astrogation_tracking_time_modifier")
+		natural_time_skip = pointersVPEMP.ConfigDriver.__get_value("VelocityPlus","VP_ENCELADUS","natural_time_skip")
 
 var this_dive_transactions_gain = 0
 var this_dive_transactions_spent = 0
 
 var show_shipped_cargo_value = true
 var astrogation_tracking_time_modifier = true
+var natural_time_skip = true
 func logEvent(type: String, details = {}):
 	.logEvent(type,details)
 	if show_shipped_cargo_value:
@@ -44,3 +47,16 @@ func deriveStats(stats):
 	var multi = astrogation_tracking_time_modifier
 	stats.CREW_STATS_ASTROGATOR_TIME = max(12 * 3600, 24 * 3600 * (stats.CREW_OCCUPATION_ASTROGATOR.experience * 30 * 3 * (multi / 90)))
 	return s
+
+
+
+func loadFromFile():
+	var age = OS.get_unix_time() - file.get_modified_time(saveFile)
+	.loadFromFile()
+	if natural_time_skip:
+		yieldTimeSkip(age)
+	
+func yieldTimeSkip(age):
+	yield(Tool.makeTimer(3, self), "timeout")
+	Debug.l("Natural Time Skip: save is %s old, advancing by %s seconds" % [timeToString(age),age])
+	advanceTimeBySeconds(age)
