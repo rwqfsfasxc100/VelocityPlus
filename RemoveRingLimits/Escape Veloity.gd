54999af64a7ca0ae8e9fc69e0f96a6cc132bdf03
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2026-09-14 23:02:27 -0700

    Updated escape velocity and leaving ring displays to use modern pointer node fetching

--- RemoveRingLimits/Escape Veloity.gd
+++ RemoveRingLimits/Escape Veloity.gd
@@ -47,7 +47,7 @@ func vp_escape_UV():
 
 func _init():
 	if CurrentGame != null:
-		pointersVPEscapeWarnVelocityOverride = CurrentGame.get_tree().get_root().get_node_or_null("HevLib~Pointers")
+		pointersVPEscapeWarnVelocityOverride = ModLoader._savedObjects[0]
 		pointersVPEscapeWarnVelocityOverride.ConfigDriver.__establish_connection("vp_escape_UV",self)
 		baseWarnVelocity = warnVelocity
 		vp_escape_UV()
--- RemoveRingLimits/Leaving Rings.gd
+++ RemoveRingLimits/Leaving Rings.gd
@@ -52,7 +52,7 @@ func vp_leavingring_UV():
 var speed_limit_config = {}
 func _init():
 	if CurrentGame != null:
-		pointersVPRingEdgeRemoval = CurrentGame.get_tree().get_root().get_node_or_null("HevLib~Pointers")
+		pointersVPRingEdgeRemoval = ModLoader._savedObjects[0]
 		pointersVPRingEdgeRemoval.ConfigDriver.__establish_connection("vp_leavingring_UV",self)
 		baseWarnVelocity = warnVelocity
 		vp_leavingring_UV()
